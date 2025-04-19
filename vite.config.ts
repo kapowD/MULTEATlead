79ac93ea4d46f81889bcd34ef92ef6a05cc965ff
Mikhail Spassky <mikhail@example.com>
2025-04-19 13:30:52 +0300

    testLanding FixedSlider1

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/MULTEATlead/', // Замените <имя-репозитория> на имя вашего репозитория
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
